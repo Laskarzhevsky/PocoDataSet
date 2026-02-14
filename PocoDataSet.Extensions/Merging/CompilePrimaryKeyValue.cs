0e7ab46e31ff3ffa8d482c3f953d8ced39fe193e
Vitaly Laskarzhevsky <vlaskarzhevsky@msn.com>
2026-02-14 12:40:18 -0500

    POCO DataSet - Serialization strategy review

--- PocoDataSet.Extensions/Merging/CompilePrimaryKeyValue.cs
+++ PocoDataSet.Extensions/Merging/CompilePrimaryKeyValue.cs
@@ -1,7 +1,5 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.Text;
 
 using PocoDataSet.IData;
 
@@ -14,68 +12,26 @@ namespace PocoDataSet.Extensions
     {
         #region Public Methods
         /// <summary>
-        /// Compiles primary key value
+        /// Compiles primary key value.
         /// </summary>
+        /// <remarks>
+        /// This method is kept for backward compatibility. New code should prefer <see cref="RowIdentityResolver.TryGetPrimaryKeyValue"/>.
+        /// </remarks>
         /// <param name="dataRow">Data row</param>
         /// <param name="primaryKeyColumnNames">Primary key column names</param>
         /// <returns>Compiled primary key value</returns>
+        [Obsolete("Use RowIdentityResolver.TryGetPrimaryKeyValue(...) instead. This extension is kept for backward compatibility.")]
         public static string CompilePrimaryKeyValue(this IDataRow? dataRow, IReadOnlyList<string> primaryKeyColumnNames)
         {
-            StringBuilder stringBuilder = new System.Text.StringBuilder();
             if (dataRow == null)
             {
-                return stringBuilder.ToString();
-            }
-
-            for (int i = 0; i < primaryKeyColumnNames.Count; i++)
-            {
-                object? primaryKeyValue = dataRow.GetDataFieldValue<object?>(primaryKeyColumnNames[i]);
-                string stringRepresentationOfValue = GetStringRepresentationOf(primaryKeyValue);
-                if (i > 0)
-                {
-                    stringBuilder.Append('|');
-                }
-
-                stringBuilder.Append(stringRepresentationOfValue.Length.ToString(CultureInfo.InvariantCulture));
-                stringBuilder.Append('#');
-                stringBuilder.Append(stringRepresentationOfValue);
-            }
-
-            return stringBuilder.ToString();
-        }
-        #endregion
-
-        #region Methods
-        /// <summary>
-        /// Gets string representation of value
-        /// </summary>
-        /// <param name="value">Value to get string representation of</param>
-        /// <returns>String representation of value</returns>
-        static string GetStringRepresentationOf(object? value)
-        {
-            if (value == null)
-            {
                 return string.Empty;
             }
 
-            if (Convert.IsDBNull(value))
-            {
-                return string.Empty;
-            }
+            string primaryKeyValue;
+            bool hasPrimaryKeyValue = RowIdentityResolver.TryGetPrimaryKeyValue(dataRow, primaryKeyColumnNames, out primaryKeyValue);
 
-            IFormattable? formattable = value as IFormattable;
-            if (formattable != null)
-            {
-                return formattable.ToString(null, CultureInfo.InvariantCulture);
-            }
-
-            string? stringRepresentationOfValue = value.ToString();
-            if (stringRepresentationOfValue == null)
-            {
-                return string.Empty;
-            }
-
-            return stringRepresentationOfValue;
+            return hasPrimaryKeyValue ? primaryKeyValue : string.Empty;
         }
         #endregion
     }
--- PocoDataSet.ObservableExtensions/ObservableDataRowExtensions/CompilePrimaryKeyValue.cs
+++ PocoDataSet.ObservableExtensions/ObservableDataRowExtensions/CompilePrimaryKeyValue.cs
@@ -1,10 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.Text;
 
-using PocoDataSet.IData;
 using PocoDataSet.IObservableData;
+using PocoDataSet.Extensions;
 
 namespace PocoDataSet.ObservableExtensions
 {
@@ -15,68 +13,26 @@ namespace PocoDataSet.ObservableExtensions
     {
         #region Public Methods
         /// <summary>
-        /// Compiles primary key value
+        /// Compiles primary key value.
         /// </summary>
+        /// <remarks>
+        /// This method is kept for backward compatibility. New code should prefer <see cref="RowIdentityResolver.TryGetPrimaryKeyValue"/>.
+        /// </remarks>
         /// <param name="observableDataRow">Observable data row</param>
         /// <param name="primaryKeyColumnNames">Primary key column names</param>
         /// <returns>Compiled primary key value</returns>
+        [Obsolete("Use RowIdentityResolver.TryGetPrimaryKeyValue(...) instead. This extension is kept for backward compatibility.")]
         public static string CompilePrimaryKeyValue(this IObservableDataRow? observableDataRow, List<string> primaryKeyColumnNames)
         {
-            StringBuilder stringBuilder = new System.Text.StringBuilder();
             if (observableDataRow == null)
             {
-                return stringBuilder.ToString();
-            }
-
-            for (int i = 0; i < primaryKeyColumnNames.Count; i++)
-            {
-                object? primaryKeyValue = observableDataRow.GetDataFieldValue<object?>(primaryKeyColumnNames[i]);
-                string stringRepresentationOfValue = GetStringRepresentationOf(primaryKeyValue);
-                if (i > 0)
-                {
-                    stringBuilder.Append('|');
-                }
-
-                stringBuilder.Append(stringRepresentationOfValue.Length.ToString(CultureInfo.InvariantCulture));
-                stringBuilder.Append('#');
-                stringBuilder.Append(stringRepresentationOfValue);
-            }
-
-            return stringBuilder.ToString();
-        }
-        #endregion
-
-        #region Methods
-        /// <summary>
-        /// Gets string representation of value
-        /// </summary>
-        /// <param name="value">Value to get string representation of</param>
-        /// <returns>String representation of value</returns>
-        static string GetStringRepresentationOf(object? value)
-        {
-            if (value == null)
-            {
                 return string.Empty;
             }
 
-            if (Convert.IsDBNull(value))
-            {
-                return string.Empty;
-            }
+            string primaryKeyValue;
+            bool hasPrimaryKeyValue = RowIdentityResolver.TryGetPrimaryKeyValue(observableDataRow.InnerDataRow, primaryKeyColumnNames, out primaryKeyValue);
 
-            IFormattable? formattable = value as IFormattable;
-            if (formattable != null)
-            {
-                return formattable.ToString(null, CultureInfo.InvariantCulture);
-            }
-
-            string? stringRepresentationOfValue = value.ToString();
-            if (stringRepresentationOfValue == null)
-            {
-                return string.Empty;
-            }
-
-            return stringRepresentationOfValue;
+            return hasPrimaryKeyValue ? primaryKeyValue : string.Empty;
         }
         #endregion
     }
